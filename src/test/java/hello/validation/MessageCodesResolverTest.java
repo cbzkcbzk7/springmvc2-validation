2247b639973a8f6806601f52266168fa11e013c1
psora <psora93@gmail.com>
2024-07-16 20:56:49 +0900

    feat:error code&message

---
+++ src/test/java/hello/validation/MessageCodesResolverTest.java
@@ -0,0 +1,37 @@
+package hello.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.ObjectError;
+
+/**
+ * packageName    : hello.validation
+ * fileName       : MessageCodesResolverTest
+ * author         : Sora
+ * date           : 2024-07-16
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-07-16        Sora       최초 생성
+ */
+public class MessageCodesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject(){
+      String[] messageCodes =  codesResolver.resolveMessageCodes("required", "item");
+        Assertions.assertThat(messageCodes).containsExactly("required.item","required");
+    }
+
+    @Test
+    void messageCodesResolverField(){
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
+    }
+}

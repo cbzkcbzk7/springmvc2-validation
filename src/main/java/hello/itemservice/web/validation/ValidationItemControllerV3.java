2d52bd89849bae1727518d1b4fae5747715325fd
psora <psora93@gmail.com>
2024-08-21 20:25:15 +0900

    feat:스프링 적용 with BeanValidation

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -10,6 +10,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -42,6 +43,27 @@ public class ValidationItemControllerV3 {
         model.addAttribute("item", new Item());
         return "validation/v3/addForm";
     }
+
+    @PostMapping("/add")
+    public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+
+        // 검증에 실패하면 다시 입력 폼으로
+        // refactoring tip : 부정의 부정은 코드 읽기가 어려우므로 메서드를 따로 빼서 만들어주는 것 추천
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}", bindingResult);
+            // bindingResult는 자동으로 넘어가기때문에 따로 model에 안담아줘도 된다.
+//            model.addAttribute("errors", errors);
+            return "validation/v3/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
 
 
     @GetMapping("/{itemId}/edit")

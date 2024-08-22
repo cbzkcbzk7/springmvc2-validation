4ce0ab3b17002decad77ca4f90cd34a3684d6f7e
psora <psora93@gmail.com>
2024-08-22 21:49:48 +0900

    feat:Groups

---
+++ src/main/java/hello/itemservice/domain/item/SaveCheck.java
@@ -0,0 +1,15 @@
+package hello.itemservice.domain.item;
+
+/**
+ * packageName    : hello.itemservice.domain.item
+ * fileName       : SaveCheck
+ * author         : Sora
+ * date           : 2024-08-22
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-08-22        Sora       최초 생성
+ */
+public interface SaveCheck {
+}

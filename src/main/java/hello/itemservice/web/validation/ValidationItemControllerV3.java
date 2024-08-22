02a03e986d0b9d1a5338094dc17e72baa59f98b9
psora <psora93@gmail.com>
2024-08-22 21:05:27 +0900

    feat:Object error

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -47,6 +47,13 @@ public class ValidationItemControllerV3 {
     @PostMapping("/add")
     public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
+        // 특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+                bindingResult.reject("totalPriceMin", new Object[]{10000,resultPrice}, null);
+            }
+        }
 
         // 검증에 실패하면 다시 입력 폼으로
         // refactoring tip : 부정의 부정은 코드 읽기가 어려우므로 메서드를 따로 빼서 만들어주는 것 추천
